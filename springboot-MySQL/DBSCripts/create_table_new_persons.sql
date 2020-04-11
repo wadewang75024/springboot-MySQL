399de5bd317da845bce575adcb506822d3c8b8a4
wadewang75024 <wadewang75024@yahoo.com>
2020-04-10 21:26:42 -0500

    change person_id to bigInt

--- springboot-MySQL/DBSCripts/create_table_new_persons.sql
+++ springboot-MySQL/DBSCripts/create_table_new_persons.sql
@@ -1,7 +1,7 @@
 -- Dont forget to add AUTO_INCREMENT to avoid "No Default value" exception in Java
 -- Use timestamp to see both date and time
 CREATE TABLE new_persons 
-(person_id int not null AUTO_INCREMENT, 
+(person_id bigInt not null AUTO_INCREMENT, 
 last_name VARCHAR(20),
 first_name VARCHAR(20), 
 address VARCHAR(100), 
@@ -16,3 +16,6 @@ delete from new_persons
 select * from new_persons
 
 drop table new_persons;
+
+insert into new_persons
+values (0, 'Wade', 'Wang', 'Test Dr', 'Test', STR_TO_DATE('2020-04-10', '%Y-%m-%d'), STR_TO_DATE('2020-04-10', '%Y-%m-%d'));
